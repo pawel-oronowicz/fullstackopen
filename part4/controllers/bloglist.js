c2a82e2b1bee6a2ab6690052063206c883156b6a
Paweloronowicz <oronowicz@gmail.com>
2024-10-21 21:41:09 +0200

    Bloglist - initial commit

---
+++ part4/controllers/bloglist.js
@@ -0,0 +1,62 @@
+const bloglistRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+bloglistRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+bloglistRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+bloglistRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+bloglistRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+bloglistRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = bloglistRouter
